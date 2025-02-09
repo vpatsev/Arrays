353a2e2f7f63149250016a9dd22d325a20feaf4a
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-02-09 03:23:34 +0200

    Add files via upload

---
+++ 04.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int odd = 0;
+            int even = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    even += number;
+                }
+                else { odd += number; }
+            }
+            Console.WriteLine(even - odd);
+        }
+    }
+}
